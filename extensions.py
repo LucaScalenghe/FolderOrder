89efca367f569f30562c7e4abc9863d1d38f2c99
LucaScalenghe <lucascalenghe1999@gmail.com>
2020-11-17 19:26:06 +0100

    this version works, but could be done better

---
+++ extensions.py
@@ -0,0 +1,122 @@
+extension_paths = {
+
+        # images
+        '.ai':     'Media/Images',
+        '.bmp':    'Media/Images',
+        '.gif':    'Media/Images',
+        '.jpg':    'Media/Images',
+        '.jpeg':   'Media/Images',
+        '.png':    'Media/Images',
+        '.ps':     'Media/Images',
+        '.psd':    'Media/Images',
+        '.svg':    'Media/Images',
+        '.tif':    'Media/Images',
+        '.tiff':   'Media/Images',
+        '.cr2':    'Media/Images',
+
+        # audio
+        '.aif':    'Media/Audio',
+        '.cda':    'Media/Audio',
+        '.mid':    'Media/Audio',
+        '.midi':   'Media/Audio',
+        '.mp3':    'Media/Audio',
+        '.mpa':    'Media/Audio',
+        '.ogg':    'Media/Audio',
+        '.wav':    'Media/Audio',
+        '.wma':    'Media/Audio',
+        '.wpl':    'Media/Audio',
+        '.m3u':    'Media/Audio',
+
+        # video
+        '.3g2':    'Media/Video',
+        '.3gp':    'Media/Video',
+        '.avi':    'Media/Video',
+        '.flv':    'Media/Video',
+        '.h264':   'Media/Video',
+        '.m4v':    'Media/Video',
+        '.mkv':    'Media/Video',
+        '.mov':    'Media/Video',
+        '.mp4':    'Media/Video',
+        '.mpg':    'Media/Video',
+        '.mpeg':   'Media/Video',
+        '.rm':     'Media/Video',
+        '.swf':    'Media/Video',
+        '.vob':    'Media/Video',
+        '.wmv':    'Media/Video',
+
+        #books
+        '.epub':   'Media/Books',
+        '.azw':    'Media/Books',
+        '.azw3':   'Media/Books',
+        '.mobi':   'Media/Books',
+
+        # text
+        '.doc':    'Text/Word',
+        '.docx':   'Text/Word',
+        '.odt ':   'Text',
+        '.pdf':    'Text/Pdf',
+        '.rtf':    'Text',
+        '.tex':    'Text',
+        '.wks':    'Text',
+        '.wps':    'Text',
+        '.wpd':    'Text',
+        # PowerPoint
+        '.key':    'Text/Presentations',
+        '.odp':    'Text/Presentations',
+        '.pps':    'Text/Presentations',
+        '.ppt':    'Text/Presentations',
+        '.pptx':   'Text/Presentations',
+        #Excel
+        '.ods':    'Text/Excel',
+        '.xlr':    'Text/Excel',
+        '.xls':    'Text/Excel',
+        '.xlsx':   'Text/Excel',
+    
+        # programming
+        '.c':      'Programming/C',
+        '.h':      'Programming/C',
+        '.class':  'Programming/Java',
+        '.java':   'Programming/Java',
+        '.py':     'Programming/Python',
+        '.sh':     'Programming/Shell',
+        '.txt':    'Programming/Txt',       #this should be in text but I use it more often with code lines
+            
+        # internet
+        '.asp':    'Other/Internet',
+        '.aspx':   'Other/Internet',
+        '.cer':    'Other/Internet',
+        '.cfm':    'Other/Internet',
+        '.cgi':    'Other/Internet',
+        '.pl':     'Other/Internet',
+        '.css':    'Other/Internet',
+        '.htm':    'Other/Internet',
+        '.js':     'Other/Internet',
+        '.jsp':    'Other/Internet',
+        '.part':   'Other/Internet',
+        '.php':    'Other/Internet',
+        '.rss':    'Other/Internet',
+        '.xhtml':  'Other/Internet',
+        '.html':   'Other/Internet',
+        # compressed
+        '.7z':     'Other/Compressed',
+        '.arj':    'Other/Compressed',
+        '.deb':    'Other/Compressed',
+        '.pkg':    'Other/Compressed',
+        '.rar':    'Other/Compressed',
+        '.rpm':    'Other/Compressed',
+        '.tar.gz': 'Other/Compressed',
+        '.z':      'Other/Compressed',
+        '.zip':    'Other/Compressed',
+        # executables
+        '.apk':    'Other/Executables',
+        '.bat':    'Other/Executables',
+        '.com':    'Other/Executables',
+        '.exe':    'Other/Executables',
+        '.gadget': 'Other/Executables',
+        '.jar':    'Other/Executables',
+        '.wsf':    'Other/Executables',
+
+         # No name uncategorized
+        'noname':  'Uncategorized'
+        
+        }
--- main.py
+++ main.py
@@ -4,16 +4,24 @@ import os
 import sys
 import time
 import logging
-import json
 import shutil
 from pathlib import Path
+from extensions import extension_paths
 
 
-def getPathName(stringA):
+def decideFolder(file_name):
+    ext = getExtension(file_name)
+    ext = '.' + ext
+    path = Path(extension_paths[ext])
+    return path
+
+
+def getName(stringA):
     a = Path(stringA)
     ris = a.name
     ris = ris.split('.')
     return ris[0]
+
 
 def getExtension(stringA):
     a = Path(stringA)
@@ -21,67 +29,68 @@ def getExtension(stringA):
     ris = ris.split('.')
     return ris[1]
 
+
 def unifyPath(stringA, stringB):
-    stringA= Path(stringA)
+    stringA = Path(stringA)
     stringB = Path(stringB)
     return stringA/stringB
 
 
 class myHandler(FileSystemEventHandler):
 
-    def __init__ (self, tracked, destination):
+    def __init__(self, tracked, destination):
         self.tracked = tracked
         self.destination = destination
 
-    def on_any_event(self,event):
-         for file_name in os.listdir(tracked):
+    def on_any_event(self, event):
+        for file_name in os.listdir(tracked):
             print(f"{file_name} has been moved to the destination folder!")
-            i=1
-            destpath = unifyPath(destination,file_name) #gives me the destination path of the file
-            
-            #here i control if the file already exists with that name
-            file_exists = os.path.isfile(unifyPath(destination, file_name))
+            i = 1
+            # gives where the file is at the moment (isn't moved yet)
+            original_path = unifyPath(tracked, file_name)   #is a string
+
+            # gives me the destination path of the file
+            # dest_path = unifyPath(destination,file_name)
+            dest = Path(destination)
+            # is a Path element that has the piece of path of the folder destinationFolderPath/folder_Path/file_name(1).extension
+            folder_path = decideFolder(file_name)
+           
+            dest_path = dest/folder_path     # is now in the right folder now you must add the file name, number and extension
+
+            dest_path = dest_path/Path(file_name)   #adding the filename.extension
+
+            file_exists = os.path.isfile(dest_path)      # check if the file already exists with that name
             while file_exists:
-                i+=1
-                # I have to add the version number of the file to not have duplicates
-                #destination + name + str(i) + extension
-                dest = Path(destination)
-                fname = getPathName(file_name)
+                #destination + name + str(i) + extension   in order to not have duplicates
+                i += 1
+                fname = getName(file_name)
                 number = str(i)
                 point = '.'
                 ext = getExtension(file_name)
-                
-                # creating the actual destination path 
-                filepath =  Path(fname + number + point + ext)
-                destpath=dest/filepath
-               
-                #qua ricontrollo se esiste perchè se esiste rifaccio il ciclo
-                file_exists = os.path.isfile(destpath)
-            
-            #qua devo spostarlo ATTENZIONE CHE IN FILE NAME DEVE ANDARE ANCHE IL numero e anche quando non ho aggiungo 
-            #nessun numero
-            os.rename(unifyPath(tracked,file_name), destpath)
 
-    
+                # creating the actual destination path adding the number
+                name_with_number = Path(fname + number + point + ext)
+                dest_path = dest_path/name_with_number
 
+                # check if not true does another while loop
+                file_exists = os.path.isfile(dest_path)
 
-
-
+            # moving the file from his original path to his destination path
+            dest_path = str(dest_path)
+            os.rename(original_path, dest_path)
 
 
 if __name__ == "__main__":
 
-
-    tracked = 'C:/Users/lucas/Desktop/Test'
-    destination = 'C:/Users/lucas/Desktop/Arrivo'
+    tracked = 'C:/Users/lucas/Desktop/BucoNero'
+    destination = 'C:/Users/lucas/Desktop/File'
 
     handler = myHandler(tracked, destination)
 
-
     obs = Observer()
-    obs.schedule(handler,tracked,recursive = False)
+    obs.schedule(handler, tracked, recursive=False)
 
-     #start the observer thread
+    # start the observer thread
     obs.start()
     try:
         while True:
@@ -90,8 +99,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         obs.stop()
         obs.join()
-
-
-
-
-
